dbac208888c626f1a149796adf728efb99855f06
Daniel Robinson <dro1510@wgu.edu>
2025-04-25 17:14:58 -0500

    testing production notes app - render

--- part3/notesapp-backend/index.js
+++ part3/notesapp-backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const cors = require("cors");
 app.use(cors());
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -24,9 +25,9 @@ let notes = [
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
